900c259fcc7657dbfaf736c4779eaea42c0c69dc
Rizky Rahadhika <rizkyrahadhika@gmail.com>
2025-01-06 12:03:21 +0700

    update

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>PPID Kota Surakarta</title>
+  </head>
+  <body>
+    <?php include 'includes/navbar.html'; ?>
+    <?php include 'includes/header.html'; ?>
+    <?php include 'includes/main.html'; ?>
+    <?php include 'includes/footer.html'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
