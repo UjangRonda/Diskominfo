c70b1ae9a661d935754b380323f117a274c83db7
Rizky Rahadhika <rizkyrahadhika@gmail.com>
2025-01-07 15:59:37 +0700

    New

--- index.php
+++ index.php
@@ -3,17 +3,28 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PPID Kota Surakarta</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
+    />
     <link rel="stylesheet" href="css/style.css" />
-    <title>PPID Kota Surakarta</title>
   </head>
   <body>
     <?php include 'includes/navbar.html'; ?>
     <?php include 'includes/header.html'; ?>
-    <?php include 'includes/main.html'; ?>
+
+    <!-- Main Content -->
+    <?php include 'main/layanan-kami.html'; ?>
+    <?php include 'main/informasi-publik.html'; ?>
+    <?php include 'main/Pusat-Informasi-Terpadu.html'; ?>
+    <?php include 'main/Keunggulan.html'; ?>
+
+  
     <?php include 'includes/footer.html'; ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
