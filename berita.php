5563f11b368b521c11053d95858b9a9cbde591d9
aaadiknh <adiknurhalimah@gmail.com>
2025-01-20 14:28:35 +0700

    berita lagee

---
+++ berita.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Berita PPID Kota Surakarta</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/animation.css" />
+  <style>
+    .berita-container .card1 {
+      border: none;
+      box-shadow: 0 5px 9px rgba(0, 0, 0, 0.1);
+      transition: transform 0.2s;
+      height: 350px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      margin-top: 70px;
+    }
+
+    .berita-container .card1:hover {
+      transform: translateY(-5px);
+    }
+
+    .berita-container .card1-body {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      flex-grow: 1;
+      padding: 15px;
+      padding-top: 10px;
+    }
+
+    .berita-container .card1-title {
+      font-size: 14px;
+      font-weight: bold;
+      margin-bottom: 10px;
+      line-height: 1.5;
+      color: grey;
+      font-family: "Poppins", sans-serif;
+    }
+
+    .berita-container .btn-read-more1 {
+      background-color: #a55a42;
+      color: white;
+      width: 45%;
+      text-align: center;
+      font-size: 16px;
+      font-weight: bold;
+      padding: 5px 0;
+      line-height: 1;
+      border-radius: 5px;
+      margin-top: auto;
+      text-decoration: none;
+      display: inline-block;
+      transition: background-color 0.2s ease;
+      font-family: "Poppins", sans-serif;
+    }
+    .berita-container .d-flex.justify-content-between {
+      margin-top: auto;
+    }
+
+    .berita-container .card1-img-top {
+      width: 100%;
+      height: 150px;
+      object-fit: cover;
+    }
+  </style>
+</head>
+
+<body>
+  <?php include 'includes/navbar.html'; ?>
+
+  <section class="berita-container">
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-md-3 mb-4">
+          <div class="card1">
+            <img
+              src="https://asset.kompas.com/crops/zc7yyQwBZD83GE_YJduI0V3mVME=/0x0:0x0/1200x800/data/photo/2025/01/09/677f86e33709e.jpg"
+              class="card1-img-top"
+              alt="Berita 1"
+            />
+            <div class="card1-body">
+              <h6 class="card1-title">
+                KPU Solo Tetapkan Respati-Astrid sebagai Wali Kota dan Wakil.....
+              </h6>
+              <div class="d-flex justify-content-between align-items-center">
+                <a
+                  href="https://regional.kompas.com/read/2025/01/09/155812778/kpu-solo-tetapkan-respati-astrid-sebagai-wali-kota-dan-wakil-wali-kota"
+                  class="btn btn-read-more1 btn-sm"
+                  >Lihat Detail</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-4">
+          <div class="card1">
+            <img
+              src="https://asset.kompas.com/crops/c2YPxwqHWyfskorIm-B0a5TrHTo=/174x0:1279x737/1200x800/data/photo/2024/10/31/6723b39a05e45.jpg"
+              class="card1-img-top"
+              alt="Berita 2"
+            />
+            <div class="card1-body">
+              <h6 class="card1-title">
+                Jadwal KRL Solo-Jogja 6-12 Januari 2025 dari Palur ke.....
+              </h6>
+              <div class="d-flex justify-content-between align-items-center">
+                <a
+                  href="https://www.kompas.com/jawa-tengah/read/2025/01/06/055800588/jadwal-krl-solo-jogja-6-12-januari-2025-dari-palur-ke-yogyakarta"
+                  class="btn btn-read-more1 btn-sm"
+                  >Lihat Detail</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-4">
+          <div class="card1">
+            <img
+              src="https://asset.kompas.com/crops/nZbDP4iXD7bojvo5ROUNqKr0H5Q=/0x0:0x0/1200x800/data/photo/2022/10/31/635fc0fe763c7.jpg"
+              class="card1-img-top"
+              alt="Berita 3"
+            />
+            <div class="card1-body">
+              <h6 class="card1-title">
+                Penumpang BST Solo Keluhkan Waktu Tunggu yang Lama, Ternyata Ini
+                Sebabnya.....
+              </h6>
+              <div class="d-flex justify-content-between align-items-center">
+                <a
+                  href="https://www.kompas.com/jawa-tengah/read/2025/01/07/131431488/penumpang-bst-solo-keluhkan-waktu-tunggu-yang-lama-ternyata-ini"
+                  class="btn btn-read-more1 btn-sm"
+                  >Lihat Detail</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 mb-4">
+          <div class="card1">
+            <img
+              src="https://asset.kompas.com/crops/neR8QPMmF76L42zVFFzTu8x2oKU=/0x0:0x0/1200x800/data/photo/2025/01/08/677e80e9e13a3.jpg"
+              class="card1-img-top"
+              alt="Berita 4"
+            />
+            <div class="card1-body">
+              <h6 class="card1-title">
+                Polisi Tangkap Pelaku Penipuan Tiket Piala AFF 2024 di Solo.....
+              </h6>
+              <div class="d-flex justify-content-between align-items-center">
+                <a
+                  href="https://regional.kompas.com/read/2025/01/08/205740578/polisi-tangkap-pelaku-penipuan-tiket-piala-aff-2024-di-solo"
+                  class="btn btn-read-more1 btn-sm"
+                  >Lihat Detail</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <?php include 'includes/footer.html'; ?>
+</body>
+
+</html>
