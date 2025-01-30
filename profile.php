8a994d7da6b2c07e4a16ff34544ffd20725b56fe
Arudian <ardiplayz21@gmail.com>
2025-01-30 09:39:13 +0700

    rev

--- profile.php
+++ profile.php
@@ -511,7 +511,6 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <?php include 'includes/footer.html'; ?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     const observerOptions = {
       root: null,
